7ca12aa7dfa6dc0e8959cf0920d5b9887da0010e
LeoSteve <zuozuo0713@gmail.com>
2017-07-16 20:40:09 +0800

    Initial Commit

---
+++ PWReminder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PWReminder
+//
+//  Created by LeoSteve on 2017/7/16.
+//  Copyright © 2017年 com。seif。PWReminder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PWReminder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PWReminder
+//
+//  Created by LeoSteve on 2017/7/16.
+//  Copyright © 2017年 com。seif。PWReminder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
